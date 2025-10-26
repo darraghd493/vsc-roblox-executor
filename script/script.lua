963a53105860e84d61533f3d745468aa3cf8f694
darraghd493 <60071719+darraghd493@users.noreply.github.com>
2025-10-26 01:35:19 +0000

    fix: early auto-exec support
    On previous exploits, this implementation was fine as LocalPlayer was already loaded. Zenith auto-executes it early enough that the player name can be nil.

--- script/script.lua
+++ script/script.lua
@@ -4,73 +4,93 @@
 ]]
 
 local CONFIG = {
-    SERVER_URL = "ws://localhost:8080";
-    DEBUG = false;
+    SERVER_URL = 'ws://localhost:8080',
+    DEBUG = false,
 }
 
 --// UTILS //
 local function fetch(url: string)
     return request({
-        Url = url;
-        Method = "GET";
+        Url = url,
+        Method = 'GET',
     }).Body
 end
 
 local function format(...)
-    if type(...) == "string" then return ... end
-    if type(...) == "number" then return tostring(...) end
-    if type(...) == "table" then return table.concat(..., " ") end
-    if type(...) == "boolean" then return tostring(...) end
-    if type(...) == "function" then return tostring(...) end
-    if type(...) == "userdata" then return tostring(...) end
-    if type(...) == "thread" then return tostring(...) end
-    if type(...) == "nil" then return "nil" end
-    return ""
+    if type(...) == 'string' then
+        return ...
+    end
+    if type(...) == 'number' then
+        return tostring(...)
+    end
+    if type(...) == 'table' then
+        return table.concat(..., ' ')
+    end
+    if type(...) == 'boolean' then
+        return tostring(...)
+    end
+    if type(...) == 'function' then
+        return tostring(...)
+    end
+    if type(...) == 'userdata' then
+        return tostring(...)
+    end
+    if type(...) == 'thread' then
+        return tostring(...)
+    end
+    if type(...) == 'nil' then
+        return 'nil'
+    end
+    return ''
 end
 
 --// CONSTANTS //
-local Logger = loadstring(fetch("https://pastebin.com/raw/MWsAEADZ"))()
+local Logger = loadstring(fetch('https://pastebin.com/raw/MWsAEADZ'))()
 
-local HttpService = game:GetService("HttpService")
-local Players = game:GetService("Players")
+local HttpService = game:GetService('HttpService')
+local Players = game:GetService('Players')
 
-local LocalPlayer = Players.LocalPlayer
+local LocalPlayer
+repeat
+    task.wait()
+    LocalPlayer = Players.LocalPlayer
+until game:IsLoaded() and LocalPlayer ~= nil
 
 --// ENUMS //
 local PACKET_ID = { -- -1 adjustment to match Node.js packet IDs
     -- Client Packets
-    CLIENT_AUTHENTICATION_REQUEST = 0x00000001;
-    CLIENT_EXECUTION_STATE = 0x00000002;
-    CLIENT_OUTPUT = 0x00000003;
+    CLIENT_AUTHENTICATION_REQUEST = 0x00000001,
+    CLIENT_EXECUTION_STATE = 0x00000002,
+    CLIENT_OUTPUT = 0x00000003,
 
     -- Server Packets
-    SERVER_AUTHENTICATION_RESPONSE = 0x10000001;
-    SERVER_EXECTUION_REQUEST = 0x10000002;
-    SERVER_ERROR = 0x10000003;
+    SERVER_AUTHENTICATION_RESPONSE = 0x10000001,
+    SERVER_EXECTUION_REQUEST = 0x10000002,
+    SERVER_ERROR = 0x10000003,
 }
 
 local EXECUTION_STATE_ID = {
-    SUCCESSFUL = 0x00000001;
-    ERROR = 0x00000002;
-    INVALID = 0x00000003;
+    SUCCESSFUL = 0x00000001,
+    ERROR = 0x00000002,
+    INVALID = 0x00000003,
 }
 
 local OUTPUT_TYPE_ID = {
-    PRINT = 0x00000001;
-    WARNING = 0x00000002;
-    ERROR = 0x00000003;
+    PRINT = 0x00000001,
+    WARNING = 0x00000002,
+    ERROR = 0x00000003,
 }
 
 local ERROR_ID = {
-    AUTHENTICATION_REQUIRED = 0x00000001;
-    INVALID_PACKET = 0x00000002;
+    AUTHENTICATION_REQUIRED = 0x00000001,
+    INVALID_PACKET = 0x00000002,
 }
 ERROR_ID.__index = ERROR_ID
 ERROR_ID.__tostring = function(self)
     if self == ERROR_ID.AUTHENTICATION_REQUIRED then
-        return "Authentication Required"
+        return 'Authentication Required'
     elseif self == ERROR_ID.INVALID_PACKET then
-        return "Invalid Packet"
+        return 'Invalid Packet'
     end
 end
 
@@ -81,14 +101,14 @@ end
     This contrasts the packet structure in the extension, but operates in a much easier way.
 ]]
 local Packet = {
-    id = 0x00000000;
-    data = {};
+    id = 0x00000000,
+    data = {},
 }
 Packet.__index = Packet
 
 function Packet:new(id, data)
     local packet = {}
-    setmetatable(packet, {__index = Packet})
+    setmetatable(packet, { __index = Packet })
     packet.id = id
     packet.data = data
     return packet
@@ -96,7 +116,7 @@ end
 
 function Packet:serialise()
     local packet = {
-        ["id"] = self.id
+        ['id'] = self.id,
     }
     for i, v in pairs(self.data) do
         packet[i] = v
@@ -108,7 +128,7 @@ function Packet:deserialise(serialised)
     local packet = HttpService:JSONDecode(serialised)
     self.id = packet.id
     for i, v in pairs(packet) do
-        if i ~= "id" then
+        if i ~= 'id' then
             self.data[i] = v
         end
     end
@@ -116,24 +136,29 @@ end
 
 --// CLIENT //
 local Client = {
-    connection = nil;
-    logger = Logger.new("vsc-roblox-executor", nil, nil, CONFIG.DEBUG and Logger.levels.DEBUG or Logger.levels.INFO);
+    connection = nil,
+    logger = Logger.new(
+        'vsc-roblox-executor',
+        nil,
+        nil,
+        CONFIG.DEBUG and Logger.levels.DEBUG or Logger.levels.INFO
+    ),
 
     -- Data
-    authenticated = false;
+    authenticated = false,
 }
 Client.__index = Client
 
 function Client.new()
     local client = {}
-    setmetatable(client, {__index = Client})
+    setmetatable(client, { __index = Client })
     return client
 end
 
 function Client:disconnect()
     if self.connection then
         self.connection:close()
-        self.logger:info("Disconnected from the script server.")
+        self.logger:info('Disconnected from the script server.')
     end
 end
 
@@ -145,7 +170,7 @@ end
 
 function Client:connect()
     self.connection = WebSocket.connect(CONFIG.SERVER_URL)
-    self.logger:debug("Listening for packets...")
+    self.logger:debug('Listening for packets...')
 
     self.connection.OnMessage:Connect(function(message)
         local packet = Packet:new()
@@ -154,10 +179,10 @@ function Client:connect()
         -- Authentication
         if not self.authenticated then
             if packet.id ~= PACKET_ID.SERVER_AUTHENTICATION_RESPONSE then
-                self.logger:error("Authentication is required.")
+                self.logger:error('Authentication is required.')
             else
                 self.authenticated = true
-                self.logger:info("Authenticated with the script server.")
+                self.logger:info('Authenticated with the script server.')
             end
             return
         end
@@ -168,41 +193,43 @@ function Client:connect()
             local validationHash = packet.data.validationHash
             local executionId = packet.data.executionId
 
-            if crypt.hash(scriptContents, "sha256") ~= validationHash then  
-                self.logger:error("Validation hash mismatch.")
+            if crypt.hash(scriptContents, 'sha256') ~= validationHash then
+                self.logger:error('Validation hash mismatch.')
                 self:send(Packet:new(PACKET_ID.CLIENT_EXECUTION_STATE, {
-                    ["state"] = EXECUTION_STATE_ID.INVALID;
-                    ["message"] = "Validation hash mismatch.";
-                    ["executionId"] = executionId;
+                    ['state'] = EXECUTION_STATE_ID.INVALID,
+                    ['message'] = 'Validation hash mismatch.',
+                    ['executionId'] = executionId,
                 }))
             end
-            self.logger:debug("Executing script with execution ID: " .. executionId)
+            self.logger:debug(
+                'Executing script with execution ID: ' .. executionId
+            )
 
             local success, err = loadstring(scriptContents)
             if err then
-                self.logger:error("Failed to execute script: " .. err)
+                self.logger:error('Failed to execute script: ' .. err)
                 self:send(Packet:new(PACKET_ID.CLIENT_EXECUTION_STATE, {
-                    ["state"] = EXECUTION_STATE_ID.ERROR;
-                    ["message"] = "";
-                    ["executionId"] = executionId;
+                    ['state'] = EXECUTION_STATE_ID.ERROR,
+                    ['message'] = '',
+                    ['executionId'] = executionId,
                 }))
                 return
             end
 
             success()
-            
+
             self:send(Packet:new(PACKET_ID.CLIENT_EXECUTION_STATE, {
-                ["state"] = EXECUTION_STATE_ID.SUCCESSFUL;
-                ["message"] = "";
-                ["executionId"] = executionId;
+                ['state'] = EXECUTION_STATE_ID.SUCCESSFUL,
+                ['message'] = '',
+                ['executionId'] = executionId,
             }))
         end
-        
+
         -- Output
         if packet.id == PACKET_ID.SERVER_ERROR then
             for i, v in pairs(ERROR_ID) do
                 if packet.data.errorId == v then
-                    self.logger:error("Server error: " .. i)
+                    self.logger:error('Server error: ' .. i)
                     return
                 end
             end
@@ -210,47 +237,53 @@ function Client:connect()
     end)
 
     self:send(Packet:new(PACKET_ID.CLIENT_AUTHENTICATION_REQUEST, {
-        ["username"] = LocalPlayer.Name,
-        ["userId"] = LocalPlayer.UserId
+        ['username'] = LocalPlayer.Name,
+        ['userId'] = LocalPlayer.UserId,
     }))
-    self.logger:info("Sent authentication request to the script server.")
+    self.logger:info('Sent authentication request to the script server.')
 
     self.connection.OnClose:Connect(function()
-        self.logger:info("Disconnected from the script server.")
+        self.logger:info('Disconnected from the script server.')
     end)
 end
 
 --// MAIN //
+if getgenv().VSC_ROBLOX_EXECUTOR_EXISTS then
+    Logger:warn("VSC Roblox Excutor (client) already exists!")
+    return
+end
+getgenv().VSC_ROBLOX_EXECUTOR_EXISTS = true
+
 local client = Client.new()
 client:connect()
 
 --// OUTPUT //
 local old = {
-    print = print;
-    warn = warn;
-    error = error;
+    print = print,
+    warn = warn,
+    error = error,
 }
 
 print = function(...)
     old.print(...)
     client:send(Packet:new(PACKET_ID.CLIENT_OUTPUT, {
-        ["type"] = OUTPUT_TYPE_ID.PRINT;
-        ["message"] = format(...);
+        ['type'] = OUTPUT_TYPE_ID.PRINT,
+        ['message'] = format(...),
     }))
 end
 
 warn = function(...)
     old.warn(...)
     client:send(Packet:new(PACKET_ID.CLIENT_OUTPUT, {
-        ["type"] = OUTPUT_TYPE_ID.WARNING;
-        ["message"] = format(...);
+        ['type'] = OUTPUT_TYPE_ID.WARNING,
+        ['message'] = format(...),
     }))
 end
 
 error = function(...)
     old.error(...)
     client:send(Packet:new(PACKET_ID.CLIENT_OUTPUT, {
-        ["type"] = OUTPUT_TYPE_ID.ERROR;
-        ["message"] = format(...);
+        ['type'] = OUTPUT_TYPE_ID.ERROR,
+        ['message'] = format(...),
     }))
 end
